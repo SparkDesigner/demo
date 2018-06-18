1f66620306ad51c82de12157552593f49cb76d29
SparkDesigner <40349530+SparkDesigner@users.noreply.github.com>
2018-06-18 02:34:08 -0400

    Update products.js

--- js/products.js
+++ js/products.js
@@ -30,86 +30,38 @@ $products = {
 		purchase : 'https://www.sparkdesigner.com/pricing/',
 		tooltip  : 'Hibachi Express & Wings House'
 	},
-
-	halfcreative : {
+	
+	palazzo_nail_bar : {
+		name     : 'Palazzo Nail Bar',
+		tag      : 'WP',
+		img      : 'https://i.imgur.com/61AClis.jpg',
+		url      : 'http://www.nailspalazzo.com/',
+		purchase : 'https://www.sparkdesigner.com/pricing/'
+	},
+	
+	linkhua : {
 		name     : 'LinkHua',
 		tag      : 'PHP',
 		img      : 'https://i.imgur.com/8jQkSq3.jpg',
 		url      : 'https://www.linkhua.com/',
-		purchase : 'http://themeforest.net/item/halfcreative-one-page-portfolio-wordpress-theme/4566909?ref=OriginalEXE',
+		purchase : 'https://www.sparkdesigner.com/pricing/',
 		tooltip  : '聚华网'
 	},
 
-	bigwig_html : {
+	ProDealTips : {
 		name     : 'BigWig',
 		tag      : 'HTML',
 		img      : 'http://i.imgur.com/rOk3oel.jpg',
 		url      : 'http://bigwig.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bigwig-modern-corporate-html5-template/4801306?ref=OriginalEXE'
+		purchase : 'https://www.sparkdesigner.com/pricing/'
 	},
 
-	hydra_wp : {
-		name     : 'Hydra',
-		tag      : 'WP',
-		img      : 'http://i.imgur.com/RR11Nw9.jpg',
-		url      : 'http://hydra.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/hydra-fullscreen-portfolio-grid-wordpress-theme/4352507?ref=OriginalEXE'
-	},
-
-	contour : {
-		name     : 'Contour',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/5RntBHM.jpg',
-		url      : 'http://contour.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/contour-minimal-portfolio-corporate-template/4750249?ref=OriginalEXE'
-	},
-
-	brandspace : {
-		name     : 'Brandspace',
-		tag      : 'WP',
-		img      : 'http://i.imgur.com/9o0TRje.jpg',
-		url      : 'http://brandspace.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/brandspace-responsive-portfolio-business-theme/3502985?ref=OriginalEXE'
-	},
-
-	bon_atelier : {
-		name     : 'Bon Atelier',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/9o0TRje.jpg',
-		url      : 'http://brandspace.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bon-atelier-responsive-one-page-html5-template/3993470?ref=OriginalEXE'
-	},
-
-	mentor_html : {
-		name     : 'Mentor',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/f0g6JU5.jpg',
-		url      : 'http://mentor.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/mentor-premium-responsive-business-html5-template/2374421?ref=OriginalEXE'
-	},
-
-	mentor_wp : {
-		name     : 'Mentor',
-		tag      : 'WP',
-		img      : 'http://i.imgur.com/8CSwoGS.jpg',
-		url      : 'http://mentor.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/mentor-premium-responsive-html5-wordpress-theme/2560147?ref=OriginalEXE'
-	},
-
-	hydra_html : {
-		name     : 'Hydra',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/qIvg1HU.jpg',
-		url      : 'http://hydra.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/hydra-fullscreen-portfolio-grid-html5-template/4158593?ref=OriginalEXE'
-	},
-
-	neo_wp : {
+	uQualis : {
 		name     : 'Neo',
 		tag      : 'WP',
 		img      : 'http://i.imgur.com/BXbOoUp.jpg',
 		url      : 'http://neo.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/neo-premium-responsive-business-wordpress-theme/2916870?ref=OriginalEXE',
+		purchase : 'https://www.sparkdesigner.com/pricing/',
 		responsive: 0
 		
 	}
